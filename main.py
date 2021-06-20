b088a6a049ce8b16a32055d8f65a051c16660425
Logan Ballard <logan.ballard@standard.com>
2021-06-20 13:07:34 -0700

    jobs working

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from db.stock_ticker_model import load_tickers_into_db, get_stock_tickers_from_d
 from stocks.stock_prices import historical_stock_data_batch, yesterdays_stock_data_batch
 from stocks.stonk_tickers import get_tickers
 
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
-def init_setup():
+
+def init_setup() -> tuple:
+    p_reg = None
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', help='increase output verbosity',
                         action='store_true')
@@ -35,41 +38,52 @@ def init_setup():
             level=logging.INFO
         )
         logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-    return args
+    if args.production:
+        p_reg = CollectorRegistry()
+    return args, p_reg
 
 
 def main():
-    args = init_setup()
+    args, prometheus_registry = init_setup()
     connection = get_database_connection(verbose=True)
     if not connection:
         sys.exit(1)
     if args.tickers:
+        g = Gauge('stock_tickers_last_successful_run', 'Last the stock ticker symbols were successfully inserted into '
+                                                       'the db', registry=prometheus_registry)
         logging.info('getting stock tickers')
         ticker_list = get_tickers()
         load_tickers_into_db(connection=connection, ticker_list=ticker_list)
         logging.info('successfully finished stock ticker population')
         if args.production:
             logging.info('Logging to prometheus - successfully populated stock tickers')
-            pass
-            # TODO populate prometheus metric
+            g.set_to_current_time()
+            push_to_gateway('http://prometheus-pushgateway.monitoring:9091/', job='stocks_job',
+                            registry=prometheus_registry)
     if args.back_populate:
+        g = Gauge('stock_back_population_last_successful_run',
+                  'Last the stock back population job was run successfully', registry=prometheus_registry)
         logging.info('running back population')
         symbol_list = get_stock_tickers_from_db(connection=connection)
         historical_stock_data_batch(connection=connection, symbol_list=symbol_list)
         logging.info('successfully finished back population')
         if args.production:
             logging.info('Logging to prometheus - successful back population')
-            pass
-            # TODO populate prometheus metric
+            g.set_to_current_time()
+            push_to_gateway('http://prometheus-pushgateway.monitoring:9091/', job='stocks_job',
+                            registry=prometheus_registry)
     if args.daily:
+        g = Gauge('stock_daily_last_successful_run',
+                  'Last the stock daily job was run successfully', registry=prometheus_registry)
         logging.info('run daily population')
         symbol_list = get_stock_tickers_from_db(connection=connection)
         yesterdays_stock_data_batch(connection=connection, symbol_list=symbol_list)
         logging.info('successfully finished daily population')
         if args.production:
             logging.info('Logging to prometheus - successfully populated daily batch job')
-            pass
-            # TODO populate prometheus metric
+            g.set_to_current_time()
+            push_to_gateway('http://prometheus-pushgateway.monitoring:9091/', job='stocks_job',
+                            registry=prometheus_registry)
 
 
 if __name__ == '__main__':
--- stocks/stock_prices.py
+++ stocks/stock_prices.py
@@ -4,7 +4,7 @@ import math
 from time import sleep
 
 import mysql.connector
-from alpha_vantage.timeseries import TimeSeries
+import requests
 
 from credentials.credentials import get_alpha_vantage_key
 from db.stock_prices_model import insert_many_stock_prices_no_ignore, insert_many_stock_prices
@@ -28,39 +28,41 @@ def get_previous_market_days_date() -> datetime:
     return prev_day
 
 
-def get_individual_symbol_historical_data(symbol: str) -> list[tuple]:
+def get_individual_symbol_historical_data_raw_api(symbol: str) -> list[tuple]:
     data_list = []
     logging.info(f'getting data from av api for {symbol}')
-    ts = TimeSeries(key=get_alpha_vantage_key())
-    try:
-        data, metadata = ts.get_daily(
-            symbol=symbol,
-            outputsize='full'
-        )
-    except ValueError as e:
-        logging.error(f'error retrieving {symbol} from AV API - {e}')
+    api_key = get_alpha_vantage_key()
+    api_str = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey={api_key}"
+    res = requests.get(api_str)
+    res_d = res.json()
+    if res.status_code > 299:
+        logging.error(f'error retrieving {symbol} from AV API')
+        logging.error(f'response code was {res.status_code}: {res.json()}')
         return []
-    for date_str, info in data.items():
+    for date_str, info in res_d.get("Time Series (Daily)", {}).items():
         data_list.append((
             symbol,
             date_str,
             info.get('3. low'),
             info.get('2. high')
         ))
-    logging.info(f'retrieved historical data from {symbol} -> sample: {data_list[0]}')
+    if data_list:
+        logging.info(f'retrieved historical data from {symbol} -> sample: {data_list[0]}')
     return data_list
 
 
-def get_prev_market_day_stock_data_individual(symbol: str, prev_day: datetime) -> tuple:
-    ts = TimeSeries(key=get_alpha_vantage_key())
-    data, metadata = ts.get_intraday(
-        symbol=symbol,
-        interval='60min',
-        outputsize='compact'
-    )
+def get_prev_market_day_stock_data_individual_raw_api(symbol: str, prev_day: datetime) -> tuple:
+    api_key = get_alpha_vantage_key()
+    api_str = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=60min&outputsize=compact&apikey={api_key}'
+    res = requests.get(api_str)
+    res_d = res.json()
+    if res.status_code > 299:
+        logging.error(f'error retrieving {symbol} from AV API')
+        logging.error(f'response code was {res.status_code}: {res.json()}')
+        return ()
     global_high = -1
     global_low = math.inf
-    for dt, values in data.items():
+    for dt, values in res_d.get('Time Series (60min)', {}).items():
         i_date = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
         if i_date.date() == prev_day.date():
             global_high = max(global_high, float(values.get('2. high', -1)))
@@ -78,7 +80,7 @@ def historical_stock_data_batch(connection: mysql.connector.MySQLConnection, sym
     data = []
     rate_limiter_counter = 5
     for s in symbol_list:
-        data.extend(get_individual_symbol_historical_data(symbol=s))
+        data.extend(get_individual_symbol_historical_data_raw_api(symbol=s))
         rate_limiter_counter -= 1
         if rate_limiter_counter == 0:
             rate_limiter_counter = 5
@@ -93,7 +95,7 @@ def yesterdays_stock_data_batch(connection: mysql.connector.MySQLConnection, sym
     prev_day = get_previous_market_days_date()
     rate_limiter_counter = 5
     for s in symbol_list:
-        data.append(get_prev_market_day_stock_data_individual(symbol=s, prev_day=prev_day))
+        data.append(get_prev_market_day_stock_data_individual_raw_api(symbol=s, prev_day=prev_day))
         rate_limiter_counter -= 1
         if rate_limiter_counter == 0:
             rate_limiter_counter = 5
